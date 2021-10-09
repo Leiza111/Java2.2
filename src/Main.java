db97b871b07bd4272ac71df4069d3893a93cc22b
Leiza Girfatova <leiza-rad@yandex.ru>
2021-10-09 20:38:27 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int initialАccount = 100;
-        int depositАmount = 3000;
-        int bonus = initialАccount + (depositАmount / 100) + depositАmount ;
-        int noBonus = initialАccount + depositАmount ;
-        if (depositАmount>=1000){
-            System.out.println (bonus);
-        } else System.out.println (noBonus);
+        int initialAccount = 100; // начальный счет
+        int depositAmount = 50; // сумма пополнения
+        int bonus;
+        bonus = depositAmount / 100; //как считает бонус
+        int totalAmount;
+        if (depositAmount >= 1000) { //если сумма пополнения >=1000
+           totalAmount = initialAccount + depositAmount + bonus;
+            System.out.println("Итоговая сумма на счету клиента - " + totalAmount + " рублей");
+            System.out.println("Бонус равен - " + bonus + " рублей");
+        } else { //иначе
+            totalAmount = initialAccount + depositAmount;
+            System.out.println("Итоговая сумма на счету клиента - " + totalAmount + " рублей");
+            System.out.println("Бонусов нет"); //вывести на консоль бонусов нет
+        }
     }
 }
-
